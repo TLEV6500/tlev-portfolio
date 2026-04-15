ba8d8dcf89174111601a9e0dfacbe17db50f1e18
TLEV <timleo.vil@gmail.com>
2026-04-15 08:05:13 +0800

    Added s25 activity code

---
+++ backend/s25/activity/index.js
@@ -0,0 +1,26 @@
+console.log("Hello World");
+// s25
+let firstName = "Tim Leobert";
+let lastName = "Villacin";
+let age = 24;
+let hobbies = [
+    "Piano and Violin",
+    "Teaching",
+    "Learning",
+    "Programming",
+    "Coffee Brewing",
+];
+let workAddress = {
+    houseNumber: "785",
+    street: "Tudela",
+    city: "Danao",
+    state: "Cebu",
+};
+
+console.log(`First Name: ${firstName}`);
+console.log(`Last Name: ${lastName}`);
+console.log(`Age: ${age}`);
+console.log(`Hobbies:`);
+console.log(hobbies);
+console.log(`Work Address:`);
+console.log(workAddress);
